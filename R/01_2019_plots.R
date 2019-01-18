020433055eba8ce6aa56f7b5719c3408f8ebe92d
Carlos Granell <canut@uji.es>
2019-01-18 14:42:43 +0100

    Figs created survey 2019

--- R/01_2019_plots.R
+++ R/01_2019_plots.R
@@ -3,6 +3,7 @@ library(likert)
 library(tidyverse)
 library(here)
 library(readxl)
+library(lubridate)
 
 file_name = "encuesta2019.xlsx"
 data_path <- here::here("data", "2019", file_name) # local file
@@ -22,21 +23,34 @@ likert_data_raw[4:8] <- lapply(likert_data_raw[4:8], factor, levels=likert_level
 likert_data_raw$turno <- sapply(X = likert_data_raw$turno, FUN = function(x) {turno_labels[x]}) 
 
 
-likert_data_raw_df <- as.data.frame(select(likert_data_raw, c("turno","p1","p2","p3","p4","p5")))
-names(likert_data_raw_df) <- c("Turno",
-                           "1. Pregunta 1", 
-                           "2. Pregunta 2",
-                           "3. Pregunta 3",
-                           "4. Pregunta 4",
-                           "5. Pregunta 5")
-
+likert_data_raw_df <- as.data.frame(select(likert_data_raw, c("fecha","turno","p1","p2","p3","p4","p5")))
+names(likert_data_raw_df) <- c("Fecha", 
+                               "Turno",
+                               "La clase me ha pasado volando", 
+                               "El caso práctica ha sido útil para entender los conceptos",
+                               "La forma de organizar el trabajo en equipo me ha gustado",
+                               "Me gustaría saber mas sobre sensores y programación",
+                               "Ha sido fácil seguir el desarrollo del proyecto")
 
 #' En caso de agrupar por "turno", https://stackoverflow.com/questions/48340901/using-likert-package-in-r-for-analyzing-real-survey-data
-likert_data_df <- likert(items=likert_data_raw_df[,c(2:6)])
-likert_data_df <- likert(items=likert_data_raw_df[,c(2:6)], grouping = likert_data_raw_df$Turno)
+likert_data_df <- likert(items=likert_data_raw_df[,c(3:7)])
+likert_data_df <- likert(items=likert_data_raw_df[,c(3:7)], grouping = factor(likert_data_raw_df$Fecha))
 
-
+summary(likert_data_df)
+       
 title <- "Encuesta 'Practica la UJI' 2019"
-plot(likert_data_df, centered = TRUE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+plot(likert_data_df, centered = FALSE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
   guides(fill=guide_legend(title=c("Respuestas"), nrow = 1))
+
+
+file_name = "encuesta2019.svg"
+data_path <- here::here("figs", file_name) # local file
+svg(filename=data_path, width=6.5, height=3, pointsize=10)
+plot(likert_data_df, centered = FALSE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+    guides(fill=guide_legend(title=c("Respuestas"), nrow = 1))
+dev.off()
+
+
+plot(likert_data_df, type = 'heat') + ggtitle(title) + 
+  theme(legend.position = 'none')
 
