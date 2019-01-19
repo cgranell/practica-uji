0f5c994cc525037845d67fc7078257c0b29938dd
Carlos Granell <canut@uji.es>
2019-01-19 16:32:15 +0100

    Plots in English

--- R/01_2019_plots.R
+++ R/01_2019_plots.R
@@ -1,22 +1,19 @@
-install.packages("likert")
 library(likert)
 library(tidyverse)
 library(here)
 library(readxl)
-library(lubridate)
 
 file_name = "encuesta2019.xlsx"
 data_path <- here::here("data", "2019", file_name) # local file
 
 #likert_data_raw <- read_excel(data_path)
-
 likert_data_raw <-  
   read_excel(data_path,
     col_types = c("date", "numeric", "text", "numeric", "numeric","numeric","numeric","numeric"),
     na = c("NA",""))
 
 likert_levels <- c(-2, -1, 0, 1, 2)
-likert_labels <- c("Muy en desacuerdo","En desacuerdo","Neutro","De acuerdo","Muy de acuerdo")
+likert_labels <- c("Muy en desacuerdo [Strognly disagree]","En desacuerdo [Disagree]","Neutro [Neutral]","De acuerdo [Agree]","Muy de acuerdo [Strongly agree]")
 turno_labels <- c("Primer: 10-12", "Segundo: 12-14")
 #likert_data_raw$turno <- factor(likert_data_raw$turno, turno_levels, turno_labels)
 likert_data_raw[4:8] <- lapply(likert_data_raw[4:8], factor, levels=likert_levels, labels=likert_labels)
@@ -26,11 +23,12 @@ likert_data_raw$turno <- sapply(X = likert_data_raw$turno, FUN = function(x) {tu
 likert_data_raw_df <- as.data.frame(select(likert_data_raw, c("fecha","turno","p1","p2","p3","p4","p5")))
 names(likert_data_raw_df) <- c("Fecha", 
                                "Turno",
-                               "La clase me ha pasado volando", 
-                               "El caso práctica ha sido útil para entender los conceptos",
-                               "La forma de organizar el trabajo en equipo me ha gustado",
-                               "Me gustaría saber mas sobre sensores y programación",
-                               "Ha sido fácil seguir el desarrollo del proyecto")
+                               "La clase me ha pasado volando [The class has flown by]", 
+                               "El caso práctica ha sido útil para entender los conceptos [The practical case has been useful to understand the concepts]",
+                               "La forma de organizar el trabajo en equipo me ha gustado [I liked the way of organizing teamwork]",
+                               "Me gustaría saber mas sobre sensores y programación [I would like to know more about sensors and programming]",
+                               "Ha sido fácil seguir el desarrollo del proyecto [It has been easy to follow the development of the project]")
+
 
 #' En caso de agrupar por "turno", https://stackoverflow.com/questions/48340901/using-likert-package-in-r-for-analyzing-real-survey-data
 likert_data_df <- likert(items=likert_data_raw_df[,c(3:7)])
@@ -38,9 +36,9 @@ likert_data_df <- likert(items=likert_data_raw_df[,c(3:7)], grouping = factor(li
 
 summary(likert_data_df)
        
-title <- "Encuesta 'Practica la UJI' 2019"
+title <- "Encuesta 'Practica la UJI' 2019 [Survey 'Practica la UJI' 2019]"
 plotlikert <- plot(likert_data_df, centered = FALSE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
-  guides(fill=guide_legend(title=c("Respuestas"), nrow = 1))
+  guides(fill=guide_legend(title=c("Respuestas [Responses]"), nrow = 1))
 plotlikert
 
 file_name = "encuesta2019-likert.svg"
@@ -55,8 +53,9 @@ png(filename=data_path, width=650, height=480, units="px", pointsize=10)
 plotlikert
 dev.off()
 
-plotheat <- plot(likert_data_df, type = 'heat') + ggtitle(title) + 
+plotheat <- plot(likert_data_df, type = 'heat') + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) +
   theme(legend.position = 'none')
+plotheat
 
 file_name = "encuesta2019-heat.svg"
 data_path <- here::here("figs", file_name) # local file
