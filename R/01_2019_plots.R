9704e3110307e31d414f661a906d4f0484ba2a6d
cgranell <canut@uji.es>
2019-02-25 12:39:33 +0100

    Update

--- R/01_2019_plots.R
+++ R/01_2019_plots.R
@@ -21,15 +21,28 @@ likert_data_raw$turno <- sapply(X = likert_data_raw$turno, FUN = function(x) {tu
 
 num_respondents <- nrow(likert_data_raw)
 
-likert_data_raw_df <- as.data.frame(select(likert_data_raw, c("fecha","turno","p1","p2","p3","p4","p5")))
-names(likert_data_raw_df) <- c("Fecha", 
-                               "Turno",
-                               "La clase me ha pasado volando [The class has flown by]", 
-                               "El caso práctica ha sido útil para entender los conceptos [The practical case has been useful to understand the concepts]",
-                               "La forma de organizar el trabajo en equipo me ha gustado [I liked the way of organizing teamwork]",
-                               "Me gustaría saber mas sobre sensores y programación [I would like to know more about sensors and programming]",
-                               "Ha sido fácil seguir el desarrollo del proyecto [It has been easy to follow the development of the project]")
+# Update: 25 Feb 2019. 
+# Flag to generate long or short name for questions. 
+short_names <- TRUE
 
+likert_data_raw_df <- as.data.frame(select(likert_data_raw, c("fecha","turno","p1","p2","p3","p4","p5")))
+if (short_names) {
+  names(likert_data_raw_df) <- c("Fecha", 
+                                 "Turno",
+                                 "P1 [Q1]", 
+                                 "P2 [Q2]",
+                                 "P3 [Q3]",
+                                 "P4 [Q4]",
+                                 "P5 [Q5]")
+} else {
+  names(likert_data_raw_df) <- c("Fecha", 
+                                 "Turno",
+                                 "La clase me ha pasado volando [The class has flown by]", 
+                                 "El caso práctica ha sido útil para entender los conceptos [The practical case has been useful to understand the concepts]",
+                                 "La forma de organizar el trabajo en equipo me ha gustado [I liked the way of organizing teamwork]",
+                                 "Me gustaría saber mas sobre sensores y programación [I would like to know more about sensors and programming]",
+                                 "Ha sido fácil seguir el desarrollo del proyecto [It has been easy to follow the development of the project]")
+}
 
 #' En caso de agrupar por "turno", https://stackoverflow.com/questions/48340901/using-likert-package-in-r-for-analyzing-real-survey-data
 likert_data_df <- likert(items=likert_data_raw_df[,c(3:7)])
